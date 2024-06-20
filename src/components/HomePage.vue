389823816d89aa09714a8ab9a93aabaa2a80009c
Mason Langer <masonlanger00@gmail.com>
2024-06-19 22:21:34 -0500

    Updates

--- src/components/HomePage.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-    import TitleParagraphWidget  from './TitleParagraphWidget.vue'
-</script>
-
-<template>
-    <div class="home">
-        <TitleParagraphWidget title="Welcome to WritingThing" paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do lebron eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim skibidi ad minim veniam, quis nostrud balls exercitation ullamco" />
-    </div>
-</template>
