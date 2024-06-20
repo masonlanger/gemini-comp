01236cab5d7f9b40d78cc58f8646fb7c9527b2be
Mason Langer <masonlanger00@gmail.com>
2024-06-19 22:06:10 -0500

    Changed vite.config...again

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+var process = {
+  env: {
+    NODE_ENV: 'production'
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/gemini-comp/',
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/gemini-comp/" : "/",
   plugins: [
     vue(),
     vueJsx(),
