27e3fc0c431c637ddcb76441efc8258157b1acfe
Mason Langer <masonlanger00@gmail.com>
2024-06-19 22:14:23 -0500

    Changes for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-var process = {
-  env: {
-    NODE_ENV: 'production'
-  }
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/gemini-comp/',
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/gemini-comp/" : "/",
   plugins: [
     vue(),
     vueJsx(),
