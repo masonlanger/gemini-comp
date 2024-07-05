2070508b34f3972f6965d8bc7ad2937940b4c77d
Mason Langer <masonlanger00@gmail.com>
2024-07-05 16:42:22 -0500

    Added focuses to Profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="home"> 
         <TitleWidget title="Profile" />
-        <h4 class="prof-username">{{ userData.username.length > 0 ? userData.username : "Set your display name"}}</h4>
+        <div class="w-1/2">
+            <h4 class="prof-username text-gray-800">{{ userData.username.length > 0 ? userData.username : "Set your display name"}}</h4>
+            <form v-if="taglineEmpty" @submit.prevent="submitTagline"><input class="prof-tagline w-full text-gray-800" type="text" placeholder="Add a placeholder" :value='tagline' @input="tagline = $event.target.value" /></form>
+            <form v-else @submit.prevent="submitTagline"><input class="prof-tagline w-full text-gray-800" type="text" :value='tagline' @input="tagline = $event.target.value" /></form>
+        </div>
+        <div class="w-1/2 flex flex-row mt-1">
+            <div v-if="userFocusesLength > 0" class="flex flex-row">
+                <div v-for="focus in userData.userFocuses" :key="focus" class="prof-focus mr-1">
+                    {{ focus }}
+                </div>
+            </div>
+            <div class="prof-add">Add focus</div>
+        </div>
         <div>
             <h4>Email: {{ userData.email }}</h4>
             <h4>Activity: Joined in {{ userData.registeredDate.toDate().toLocaleString('default', { month: 'long', year: 'numeric' }) }}, last seen {{userData.lastLogin.toDate().toLocaleString('default', { month: 'long', day: 'numeric', year: 'numeric' })}}</h4>
@@ -13,12 +25,22 @@
 import TitleWidget from '@/components/TitleWidget.vue';
 import { getAuth } from 'firebase/auth';
 import { db } from '@/firebaseConfig';
-import { getDoc, doc } from 'firebase/firestore';
+import { getDoc, updateDoc, doc } from 'firebase/firestore';
+import { ref } from 'vue';
 
 
 const currUid = getAuth().currentUser.uid;
 const docRef = doc(db, "users", currUid);
-const userInfo = await getDoc(docRef);
-const userData = userInfo.data();
+const userData = await getDoc(docRef).then(doc => doc.data());
+const tagline = userData.tagline != null && userData.tagline.length > 0 ? ref(userData.tagline) : ref('');
+const taglineEmpty = tagline.value.length == 0;
+const userFocusesLength = userData.userFocuses.length;
+
+const submitTagline = () => {
+    const docRef = doc(db, "users", currUid);
+    updateDoc(docRef, {
+        tagline: tagline.value
+    });
+}
 
 </script>
