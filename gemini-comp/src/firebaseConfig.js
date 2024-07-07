13855063141608b250c8253aa9b4d7b4c1d6dfe9
Mason Langer <masonlanger00@gmail.com>
2024-07-07 14:02:34 -0500

    More profile additions, added firebase storage bucket

--- gemini-comp/src/firebaseConfig.js
+++ gemini-comp/src/firebaseConfig.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { collection, getFirestore } from 'firebase/firestore';
+import { getStorage, ref } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,10 +15,11 @@ const firebaseConfig = {
   storageBucket: "writing-fellow.appspot.com",
   messagingSenderId: "93130071094",
   appId: "1:93130071094:web:137a6a6d4defd98e898c97",
-  measurementId: "G-C45TTXCC59"
+  measurementId: "G-C45TTXCC59",
 };
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
+export const storage = getStorage();
 export const users = collection(db, 'users');
--- gemini-comp/src/views/ProfileView.vue
+++ gemini-comp/src/views/ProfileView.vue
@@ -1,22 +1,46 @@
 <template>
     <div class="home"> 
         <TitleWidget title="Profile" />
-        <div class="w-1/2">
-            <h4 class="prof-username text-gray-800">{{ userData.username.length > 0 ? userData.username : "Set your display name"}}</h4>
-            <form v-if="taglineEmpty" @submit.prevent="submitTagline"><input class="prof-tagline w-full text-gray-800" type="text" placeholder="Add a placeholder" :value='tagline' @input="tagline = $event.target.value" /></form>
-            <form v-else @submit.prevent="submitTagline"><input class="prof-tagline w-full text-gray-800" type="text" :value='tagline' @input="tagline = $event.target.value" /></form>
-        </div>
-        <div class="w-1/2 flex flex-row mt-1">
-            <div v-if="userFocusesLength > 0" class="flex flex-row">
-                <div v-for="focus in userData.userFocuses" :key="focus" class="prof-focus mr-1">
-                    {{ focus }}
+        <div class="row w-full items-center">
+            <input type="file" ref="file" style="display: none" @change="uploadUserImg" @input="userImg = $event.target.value"/>
+            <div @click="$refs.file.click()" class="prof-img flex justify-center items-center">
+                <font-awesome-icon :icon="['fas', 'user']" class="fa-3x text-gray-500" />
+            </div>
+            <div>
+                <div class="w-full">
+                    <div class="flex flex-row items-center space-x-2">
+                        <h4 class="prof-username text-gray-800">{{ userData.username.length > 0 ? userData.username : "Set your display name"}}</h4>
+                        <div v-if="userData.userType" class="pill">{{ userData.userType }}</div>
+                    </div>
+                    <form v-if="taglineEmpty" @submit.prevent="submitTagline"><input class="prof-tagline w-full text-gray-800" type="text" placeholder="Add a placeholder" :value='tagline' @input="tagline = $event.target.value" /></form>
+                    <form v-else @submit.prevent="submitTagline"><input class="prof-tagline w-full text-gray-800" type="text" :value='tagline' @input="tagline = $event.target.value" /></form>
+                </div>
+                <div class="w-full flex flex-row mt-1">
+                    <div v-if="userFocusesLength > 0" class="flex flex-row">
+                        <div v-for="focus in userData.userFocuses" :key="focus" @click="removeFocus(focus)" class="prof-focus mr-1">
+                            {{ focus[0].toUpperCase() + focus.slice(1)}}
+                        </div>
+                    </div>
+                    <div v-if="!showFocusOptions" class="prof-add" @click="toggleFocusOptions">Add focus</div>
+                    <div v-else class="prof-add" @click="toggleFocusOptions">Hide options</div>
+                </div>
+                <div v-if="showFocusOptions" class="w-full mt-2">
+                    <div class="flex flex-row">
+                        <div v-for="focus in focusOptions" :key="focus">
+                            <div v-if="!userData.userFocuses.includes(focus)" @click="addFocus(focus)" class="prof-focus mr-1">
+                                {{ focus[0].toUpperCase() + focus.slice(1)}}
+                            </div>
+                            <div v-else class="prof-add mr-1">
+                                {{ focus[0].toUpperCase() + focus.slice(1)}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h4>Email: {{ userData.email }}</h4>
+                    <h4>Activity: Joined in {{ userData.registeredDate.toDate().toLocaleString('default', { month: 'long', year: 'numeric' }) }}, last seen {{userData.lastLogin.toDate().toLocaleString('default', { month: 'long', day: 'numeric', year: 'numeric' })}}</h4>
                 </div>
             </div>
-            <div class="prof-add">Add focus</div>
-        </div>
-        <div>
-            <h4>Email: {{ userData.email }}</h4>
-            <h4>Activity: Joined in {{ userData.registeredDate.toDate().toLocaleString('default', { month: 'long', year: 'numeric' }) }}, last seen {{userData.lastLogin.toDate().toLocaleString('default', { month: 'long', day: 'numeric', year: 'numeric' })}}</h4>
         </div>
     </div>
 </template>
@@ -35,12 +59,44 @@ const userData = await getDoc(docRef).then(doc => doc.data());
 const tagline = userData.tagline != null && userData.tagline.length > 0 ? ref(userData.tagline) : ref('');
 const taglineEmpty = tagline.value.length == 0;
 const userFocusesLength = userData.userFocuses.length;
+const userImg = userData.userImg != null ? ref(userData.userImg) : ref('');
+
+const focusOptions = await getDoc(doc(db, "assets", "focus-options")).then(doc => doc.data().options);
 
 const submitTagline = () => {
     const docRef = doc(db, "users", currUid);
     updateDoc(docRef, {
         tagline: tagline.value
     });
+}
+
+const showFocusOptions = ref(false);
+const toggleFocusOptions = () => {
+    console.log("Clicked");
+    showFocusOptions.value = !showFocusOptions.value;
+}
+
+const addFocus = (focus) => {
+    if (userData.userFocuses.includes(focus)) return;
+    userData.userFocuses.push(focus);
+    const docRef = doc(db, "users", currUid);
+    updateDoc(docRef, {
+        userFocuses: userData.userFocuses
+    });
+}
+
+const removeFocus = (focus) => {
+    if (!userData.userFocuses.includes(focus)) return;
+    userData.userFocuses = userData.userFocuses.filter(f => f != focus);
+    const docRef = doc(db, "users", currUid);
+    updateDoc(docRef, {
+        userFocuses: userData.userFocuses.filter(f => f != focus)
+    });
+}
+
+const uploadUserImg = (event) => {
+    console.log("Uploading user image");
+    console.log(event.target.files[0]);
 }
 
 </script>
