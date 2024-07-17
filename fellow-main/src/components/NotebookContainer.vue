bd755b4de90ddff1cfbb1791b8c57f9b07403ae1
Mason Langer <masonlanger00@gmail.com>
2024-07-16 21:31:19 -0500

    Added wrapping in Library for notebook, changed navbar, removed media query

--- fellow-main/src/components/NotebookContainer.vue
+++ fellow-main/src/components/NotebookContainer.vue
@@ -9,14 +9,22 @@
     
     const currUid = getAuth().currentUser.uid;
     const notebooks = useCollection(collection(db, "users", currUid, "notebooks" ));
-
+    // const notebookRows = Math.ceil(notebooks.length/5);
+    // console.log(notebookRows)
 </script>
 
 <template>
-    <div class="grid-box">
+    <div class="row">
             <AddNewNotebook />
             <div v-if="notebooks.length > 0">
-                <NotebookThumbnail :edittable=true v-for="notebook in notebooks" :notebook="notebook" :key="notebook.id"/>
+                <div v-for="n in Math.ceil(notebooks.length/5)" :key="n">
+                    <div v-if="n == 1" class="row"> 
+                        <NotebookThumbnail :edittable=true v-for="notebook in notebooks.slice(n-1, 5)" :notebook="notebook" :key="notebook.id"/>
+                    </div>
+                    <div v-else class="row mt-1">
+                        <NotebookThumbnail :edittable=true v-for="notebook in notebooks.slice(5*(n-1), notebooks.length-(5*(n-1)) > 0 ? 5*(n-1)+5 : notebooks.length)" :notebook="notebook" :key="notebook.id"/>
+                    </div>
+                </div>
             </div>
             <div v-else class="flex flex-col content-center items-center p-2 text-gray-500" style="font-size: 20pt; font-weight: 250; width: 20rem; height: 100%;">
                 <h3>Start your Fellow-powered writing journey by creating your first Notebook!</h3>
