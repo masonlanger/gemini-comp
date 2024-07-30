9923428404d9123184b6c6560fe47c62993ad503
WylieBlondies <wylie@blondiestreehouse.com>
2024-07-30 12:13:51 -0400

    library

---
+++ fellow-main/src/components/PublishedThumbnail.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps({
+    notebook: {
+        type: Object,
+        required: true
+    },
+    zIndex: {
+        type: Number,
+        default: 1,
+        required: true
+    
+    }
+})
+
+function onThumbnailClick() {
+    window.open('http://localhost:3030/?' + props.notebook.id, "_blank");
+}
+
+</script>
+
+
+
+
+
+<template>
+    <div class="notebook-thumbnail rounded" :style="{ 'z-index': zIndex }">
+        <div class="notebook-thumbnail--flex" @click="onThumbnailClick">
+            <div class="items-center space-x-1 text-gray-700">
+                <font-awesome-icon class="fa-8x" :icon="['fas', 'book-open']" />
+                <br/>
+                <h2 class="notebook-thumbnail--title">{{ notebook.title }}</h2>
+            </div>
+        </div>
+    </div>
+</template>
--- fellow-main/src/views/DashboardView.vue
+++ fellow-main/src/views/DashboardView.vue
@@ -1,32 +1,36 @@
 <script setup>
-    import TitleWidget from '../components/TitleWidget.vue'
-    import { getAuth,onAuthStateChanged } from 'firebase/auth'
-    import { useRouter } from 'vue-router'
-    import { onBeforeUnmount } from 'vue'
     import { ref } from 'vue'
     import { useCollection } from 'vuefire'
     import { db } from '@/firebaseConfig';
     import { collection, orderBy } from 'firebase/firestore';
     import { query } from 'firebase/firestore';
-    import NotebookThumbnail from '../components/NotebookThumbnail.vue'
+    import PublishedThumbnail from '../components/PublishedThumbnail.vue'
     
     const publishedRow = ref(Math.max(Math.floor((window.innerWidth-256)/(15*16)), 1));
-    const currUid = getAuth().currentUser.uid;
     const stories = useCollection(query(collection(db, "published"), orderBy("updated", "desc")));
     window.addEventListener('resize', () => {
         publishedRow.value = Math.max(Math.floor((window.innerWidth-256)/(15*16)), 1);
     });
+
+    window.addEventListener('resize', () => {
+        publishedRow.value = Math.max(Math.floor((window.innerWidth-256)/(15*16)), 1);
+    });
+
 </script>
 
 <template>
-    <div v-if="stories.length > 0">
-                <div v-for="n in Math.ceil(stories.length/publishedRow)" :key="n">
-                    <div v-if="n == 1" class="row space-x-4"> 
-                        <NotebookThumbnail :edittable=true v-for="story in stories.slice(n-1, publishedRow)" :zIndex="stories.length-stories.indexOf(story)+2" :story="story" :key="story.id"/>
-                    </div>
-                    <div v-else class="row mt-1 space-x-4">
-                        <NotebookThumbnail :edittable=true v-for="story in stories.slice(publishedRow*(n-1), stories.length-(publishedRow*(n-1)) > 0 ? publishedRow*(n-1)+publishedRow : stories.length)" :zIndex="stories.length-stories.indexOf(story)+2" :story="story" :key="story.id"/>
-                    </div>
+    <div v-if="stories.length > 0" class="story-container">
+        <div v-for="n in Math.ceil(stories.length/publishedRow)" :key="n">
+            <div v-if="n == 1" class="row space-x-4"> 
+                <div v-for="story in stories.slice(n-1, publishedRow)" :zIndex="stories.length-stories.indexOf(story)+2" :notebook="story" :key="story.id">
+                    <PublishedThumbnail :notebook="story" :zIndex=1 />
                 </div>
             </div>
+            <div v-else class="row mt-1 space-x-4">
+                <div v-for="story in stories.slice(publishedRow*(n-1), stories.length-(publishedRow*(n-1)) > 0 ? publishedRow*(n-1)+publishedRow : stories.length)" :zIndex="stories.length-stories.indexOf(story)+2" :notebook="notebook" :key="story.id">
+                    <PublishedThumbnail :notebook="story" :zIndex=1 />
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
